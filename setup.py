35c78b73550c37d93e581f5e487e18914d49e2fe
Dusan Markovic <dusan.markovic@htecgroup.com>
2021-10-09 21:17:35 +0200

    added TravelCalculator

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='travel-calculator',
-    version='1.0.1',
+    version='1.0.2',
     description='Handle travel salesman problem',
     author='Dusan Markovic',
     author_email='dusan91judo@gmail.com',
     url='https://github.com/dusan91judo/travel_calulator',
-    packages=find_packages(exclude=('tests*', ))
+    packages=find_packages(exclude=('tests*', )),
+    install_requires=[
+        'ortools==9.1.9490'
+    ]
 )
