d60edc510967cc963869ad53085e1525440902e0
Dusan Markovic <dusan.markovic@htecgroup.com>
2021-10-10 17:08:53 +0200

    fix setup url

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='travel-calculator',
-    version='1.0.6',
+    version='1.0.7',
     description='Handle travel salesman problem',
     author='Dusan Markovic',
     author_email='dusan91judo@gmail.com',
-    url='https://github.com/dusan91judo/travel_calulator',
+    url='https://github.com/dusan91judo/travel_calculator',
     packages=find_packages(exclude=('tests*', )),
     install_requires=[
         'ortools==9.1.9490'
